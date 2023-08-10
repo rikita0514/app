ab165fd8341bf94526fc8b8a6a943ece086d5257
rikita0514 <rikitamondal0514@gmail.com>
2023-08-11 01:10:55 +0530

    dom-li-edt

--- dom.js
+++ dom.js
@@ -7,10 +7,18 @@
 // title.style.color = '#008000';
 // title.style.fontWeight='900';
  
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+// items[2].style.backgroundColor='#00b300';
+var li = document.getElementsByTagName('li');
+console.log(li);
+
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
+    li[i].style.backgroundColor='lightgrey';
 }
-items[2].style.backgroundColor='#00b300';
+li[2].style.backgroundColor='#00b300';
