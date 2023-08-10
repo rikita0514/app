627402cfe80937b953fc5de16fb0faeb9e1ca5df
rikita0514 <rikitamondal0514@gmail.com>
2023-08-11 00:49:29 +0530

    dom-class-edt

--- dom.js
+++ dom.js
@@ -1,9 +1,16 @@
 
 
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-var title = document.getElementById('title');
-headerTitle.style.borderBottom='solid 3px #000';
-title.style.color = '#008000';
-title.style.fontWeight='900';
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// var title = document.getElementById('title');
+// headerTitle.style.borderBottom='solid 3px #000';
+// title.style.color = '#008000';
+// title.style.fontWeight='900';
  
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+items[2].style.backgroundColor='#00b300';
