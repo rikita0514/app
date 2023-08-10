17488f60c94ab91e9023088543a66446f07c21ad
rikita0514 <rikitamondal0514@gmail.com>
2023-08-10 14:25:27 +0530

    new-form-dom

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+var title = document.getElementById('title');
+headerTitle.style.borderBottom='solid 3px #000';
+title.style.color = '#008000';
+title.style.fontWeight='900';
+ 
