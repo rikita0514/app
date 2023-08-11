a6223d0f218463fa103b3fe630a21dc180807759
rikita0514 <rikitamondal0514@gmail.com>
2023-08-11 13:03:32 +0530

    dom-queryselector-edt

--- dom.js
+++ dom.js
@@ -14,11 +14,25 @@
 //     items[i].style.fontWeight='bold';
 // }
 // items[2].style.backgroundColor='#00b300';
-var li = document.getElementsByTagName('li');
-console.log(li);
+// var li = document.getElementsByTagName('li');
+// console.log(li);
 
-for(let i=0;i<li.length;i++){
-    li[i].style.fontWeight='bold';
-    li[i].style.backgroundColor='lightgrey';
-}
-li[2].style.backgroundColor='#00b300';
+// for(let i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+//     li[i].style.backgroundColor='lightgrey';
+// }
+// li[2].style.backgroundColor='#00b300';
+var header= document.querySelector('#main-header');
+header.style.borderBottom= 'solid 4px black';
+
+var input = document.querySelector('input');
+input.value='Enter Your Item';
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value='Add';
+
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='none';
